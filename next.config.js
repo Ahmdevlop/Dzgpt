018e18d9632e5d3017de140b6e39ed38482ddae0
Ahmdevlop <ayadabderrahmane01@gmail.com>
2025-07-21 19:59:09 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+git init
+git remote add origin https://github.com/ahmdevlop/DZGPT.git
+git add .
+git commit -m "Initial commit"
+git push -u origin master
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
